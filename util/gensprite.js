015c9d4ef2bdede2059189630bfb84f241c86458
Alex Webb <alexwebb2@gmail.com>
2017-04-16 09:45:04 -0400

    Added dummy 2x sprites

--- util/gensprite.js
+++ util/gensprite.js
@@ -4,49 +4,70 @@ let fs = require('fs')
 
 let dir = 'images/sprite-src'
 
-fs.readdir(dir, (err, results) => {
-    if (err) {throw err}
+main()
 
-    var sprites = _(results).chain()
-        .filter(f => {
-            return f.endsWith('.png')
-        })
-        .filter(f => {
-            if (['water3.png', 'water2.png', 'water.jpg'].includes(f)) {
-                return false
-            }
-
-            return true
-        })
-        .map(f => {
-            return dir + '/' + f
-        })
-        .value()
-
-    spritesmith.run({src: sprites}, (err, result) => {
+async function main() {
+    fs.readdir(dir, (err, results) => {
         if (err) {throw err}
 
-        let data = _(result.coordinates).chain()
-            .each(c => {
-                c.pixelRatio = 1
+        var sprites = _(results).chain()
+            .filter(f => {
+                return f.endsWith('.png')
             })
-            .toPairs()
-            .map(pair => {
+            .filter(f => {
+                if (['water3.png', 'water2.png', 'water.jpg'].includes(f)) {
+                    return false
+                }
 
-                return [pair[0].replace('images/sprite-src/', '').replace('.png', ''), pair[1]]
+                return true
             })
-            .fromPairs()
+            .map(f => {
+                return dir + '/' + f
+            })
             .value()
 
-        fs.writeFile('./images/sprite.json', JSON.stringify(data, null, 4), 'utf8', err => {
-           if (err) {throw err}
-            console.log('wrote sprite.json')
+        spritesmith.run({src: sprites}, async function(err, result) {
+            if (err) {throw err}
 
-            fs.writeFile('./images/sprite.png', result.image, err => {
-                if (err) {throw err}
-                console.log('wrote sprite.png')
-                process.exit()
-            })
+            let data = _(result.coordinates).chain()
+                .each(c => {
+                    c.pixelRatio = 1
+                })
+                .toPairs()
+                .map(pair => {
+                    return [pair[0].replace('images/sprite-src/', '').replace('.png', ''), pair[1]]
+                })
+                .fromPairs()
+                .value()
+
+            await writeFile('./images/sprite.json', JSON.stringify(data, null, 4), 'utf8')
+            await writeFile('./images/sprite.png',  result.image)
+
+            await writeFile('./images/sprite@2x.json', JSON.stringify(data, null, 4), 'utf8')
+            await writeFile('./images/sprite@2x.png',  result.image)
+
+            console.log()
+            console.log('Done!')
+            console.log()
         })
     })
-})
+}
+
+async function writeFile(filePath, data, encoding) {
+    return new Promise((resolve, reject) => {
+        if (encoding) {
+            fs.writeFile(filePath, data, encoding, (err, res) => {
+                if (err) {return reject(err)}
+                console.log('wrote ' + filePath)
+                resolve(res)
+            })
+        }
+        else {
+            fs.writeFile(filePath, data, (err, res) => {
+                if (err) {return reject(err)}
+                console.log('wrote ' + filePath)
+                resolve(res)
+            })
+        }
+    })
+}
