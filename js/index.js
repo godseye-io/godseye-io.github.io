8550933fea78f57a3d6ed2eaf813ad40cec23ff6
Alex Webb <alexwebb2@gmail.com>
2017-04-19 21:17:57 -0400

    Version bump 0.0.3

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@
 // var bounds = [[0,0], [1000,1000]];
 // var image = L.imageOverlay('uqm_map_full.png', bounds).addTo(map);
 
-window.api_version = '0.0.2'
+window.api_version = '0.0.3'
 $('#version').text(window.api_version)
 
 window.queryParams = getQueryParams()
